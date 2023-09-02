1633a4cb8d844558a56f1ba1669f75112c0b21d2
HungFai Koo <jhk888@yahoo.com>
2023-09-02 11:03:30 -0700

    task 3 and 4 completed - not sure task 3 and task 4 correct

--- algorithim_hw3.py
+++ algorithim_hw3.py
@@ -1,4 +1,3 @@
-
 ## Algorithim 3
 
 # Task 1
@@ -34,14 +33,35 @@ print(invert_list(list1))
 # Task 3
 
 
-# def max_diff(arr: list):
+def max_diff(arr: list):
+    # if len(list1) == 0:
+    #     return 0
+        list2 = sorted(list1)
+        num1 = list2[-1] - list2[0]
+        return num1
+
+list1 = [3, 5, 7, 2]
+
+
+print(max_diff(list1))
 
 
 
 # Task 4
 
-# def count_larger_neighbors(arr: list):
+def count_larger_neighbors(arr: list):
+    count = 0
+    if len(list1) < 3:
+        return count
+    for i in range(1, len(list1) - 2):
+        if list1[i] > list1[i - 1] and list1[i] > list1[i + 1]:
+            count += 1
+    return count
 
+list1 = [2, 56, 7, 21, 22, 19, 26]
+result = count_larger_neighbors(list1)
+
+print(result)
 
 
 
@@ -51,7 +71,7 @@ print(invert_list(list1))
 def subtract_min(arr: list):
     list2 = []
     num = min(list1)
-    print(num)
+    # print(num)
 
     for n in list1:
         n -= 1
